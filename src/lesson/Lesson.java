67d5d52dd00dec49b57228a44fe9da6e8b80793e
MirekTuruk <miroslaw.turuk@gmail.com>
2019-01-28 22:44:39 +0100

    Initial commit

---
+++ src/lesson/Lesson.java
@@ -0,0 +1,27 @@
+package lesson;
+
+public class Lesson
+{
+    public static void main(String[] args)
+    {
+        int x = 0;
+        int tab[] = new int[5];
+        tab[0] = 1;
+        tab[1] = 2;
+        tab[2] = 3;
+        tab[3] = 4;
+        tab[4] = 5;
+
+        int tab2[] = new int[]{5, 6, 7, 8, 9};
+        String tab3[] = new String[]{"aaa", "bbb", "ccc"};
+
+        for (int y = 0; y < tab3.length; y++)
+            System.out.println(y + " : " + tab3[y]);
+
+        while (x < tab3.length)
+        {
+            System.out.println(tab3[x]);
+            x++;
+        }
+    }
+}
